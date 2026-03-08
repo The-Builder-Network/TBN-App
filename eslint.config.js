6aa2c2fac50ccb894dfb32d8c7696695ef6b6049
Sarthak S Kumar <sarthakskumar7@gmail.com>
2026-03-08 23:51:58 +0530

    refactor(misc): refactor integration layer with minor design tweaks

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ['dist/*'],
+  },
+  {
+    rules: {
+      'react/display-name': 'off',
+      'react/no-unescaped-entities': 'off',
+    },
+  },
+]);
