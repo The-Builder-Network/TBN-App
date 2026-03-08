7fadf392c8ec2732dfdaa6357b86aca15d5fab9f
Sarthak S Kumar <sarthakskumar7@gmail.com>
2026-03-08 23:52:26 +0530

    refactor(misc): update data models for better stability

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' })
+// Fix pnpm + Windows node_modules resolution
+config.resolver.nodeModulesPaths = [path.resolve(__dirname, 'node_modules')];
+
+module.exports = withNativeWind(config, { input: './global.css' });
